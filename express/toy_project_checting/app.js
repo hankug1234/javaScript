1de75358027f093f8929abfa67af1e499a02d360
hankug1234 <hankug12@gmail.com>
2022-08-25 20:17:48 +0900

    today study

---
+++ express/toy_project_checting/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cookieParser  = require('cookie-parser');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const session = require('express-session');
+
+dotenv.config();
+const pageRouter = require("./routes/?");
+
+app = express();
+app.set('port',process.env.PORT | 3003);
+app.set('view engine','html');
+nunjucks.configure('views',{express: app, watch:true});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave:false,
+  saveUninitialized:false,
+  secret:process.env.COOKIE_SECRET,
+  cookie:{
+    httpOnly:true,
+    secure:false,
+  }
+}));
+
+app.use('/',pageRouter);
+
+app.use((req,res,next)=>{
+  const error = new Error(`${req.method} ${req.url} no router`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err,req,res,next)=>{
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'projection' ? err : {};
+  res.status(error.status | 500);
+  res.render('error');
+})
+
+app.listen(app.get('port'),()=>{console.log(app.get('port','ready'))});
